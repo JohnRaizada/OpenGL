fc563e11bca3b6dd6aabfbbaf4c09b3f67c0c835
PracticeApp1 <indusinghkanchan@outlook.com>
2023-04-08 01:34:59 +0530

    Buffer Layout Abstraction in OpenGL

---
+++ OpenGL/src/Renderer.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <GL/glew.h>
+#define ASSERT(x) if (!(x)) __debugbreak();
+#ifdef NDEBUG
+#define GLCall(x) x;
+#else
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+#endif
+void GLClearError();
+bool GLLogCall(const char* function, const char* file, int line);
