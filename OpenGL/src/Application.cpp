f948e60036c1405b3d1b2186907151c5256cb141
PracticeApp1 <indusinghkanchan@outlook.com>
2023-04-08 21:28:02 +0530

    ImGui in OpenGL

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -13,6 +13,8 @@
 #include "Texture.h"
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
 int main(void)
 {
 	GLFWwindow* window;
@@ -58,12 +60,10 @@ int main(void)
 		va.AddBuffer(vb, layout);
 		IndexBuffer ib(indices, 6);
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-		glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
-		glm::vec4 result = proj * vp;
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", proj);
 		Texture texture("res/textures/Screenshot.png");
 		texture.Bind();
 		shader.SetUniform1i("u_Texture", 0);
@@ -72,6 +72,10 @@ int main(void)
 		ib.Unbind();
 		shader.Unbind();
 		Renderer renderer;
+		ImGui::CreateContext();
+		ImGui_ImplGlfwGL3_Init(window, true);
+		ImGui::StyleColorsDark();
+		glm::vec3 translation(200, 200, 0);
 		float r = 0.0f;
 		float increment = 0.05;
 		/* Loop until the user closes the window */
@@ -79,14 +83,24 @@ int main(void)
 		{
 			/* Render here */
 			renderer.Clear();
+			ImGui_ImplGlfwGL3_NewFrame();
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+			glm::mat4 mvp = proj * view * model;
 			shader.Bind();
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+			shader.SetUniformMat4f("u_MVP", mvp);
 			renderer.Draw(va, ib, shader);
 			if (r > 1.0f)
 				increment = -0.05;
 			else if (r < 0.0f)
 				increment = 0.05f;
-			r += increment;
+			r += increment; 
+			{                         // Display some text (you can use a format string too)
+				ImGui::SliderFloat3("Trnslation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			}
+			ImGui::Render();
+			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 			/* Swap front and back buffers */
 			glfwSwapBuffers(window);
 
@@ -94,6 +108,8 @@ int main(void)
 			glfwPollEvents();
 		}
 	}
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
 	glfwTerminate();
 	return 0;
 }
