dc1c074bb71e2d671b9ab554a640a410a7c2aabe
PracticeApp1 <indusinghkanchan@outlook.com>
2023-04-08 20:32:05 +0530

    Projection Matrices in OpenGL

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -39,10 +39,10 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
 	{
 		float positions[] = {
-			-0.5f, -0.5f, 0.0f, 0.0f,	//0
-			 0.5f, -0.5f, 1.0f, 0.0f,	//1
-			 0.5f,  0.5f, 1.0f, 1.0f,	//2
-			-0.5f,  0.5f, 0.0f, 1.0f	//3
+			100.0f, 100.0f, 0.0f, 0.0f,	//0
+			200.0f, 100.0f, 1.0f, 0.0f,	//1
+			200.0f, 200.0f, 1.0f, 1.0f,	//2
+			100.0f, 200.0f, 0.0f, 1.0f	//3
 		};
 		unsigned int indices[] = {
 			0,1,2,
@@ -57,7 +57,9 @@ int main(void)
 		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
 		IndexBuffer ib(indices, 6);
-		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
+		glm::vec4 result = proj * vp;
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
