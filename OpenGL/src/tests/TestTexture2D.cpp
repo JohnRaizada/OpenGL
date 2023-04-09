23993f1e68b772d292c32ee659bf91c1d893b367
PracticeApp1 <indusinghkanchan@outlook.com>
2023-04-09 15:13:57 +0530

    Batch Renderering and series conclusion

--- OpenGL/src/tests/TestTexture2D.cpp
+++ OpenGL/src/tests/TestTexture2D.cpp
@@ -8,24 +8,32 @@ namespace test
 	TestTexture2D::TestTexture2D():m_Proj(glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f)), m_View(glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0))),m_TranslationA(200,200,0),m_TranslationB(400,200,0)
 	{
 		float positions[] = {
-			   -50.0f, -50.0f, 0.0f, 0.0f,	//0
-				50.0f, -50.0f, 1.0f, 0.0f,	//1
-				50.0f,  50.0f, 1.0f, 1.0f,	//2
-			   -50.0f,  50.0f, 0.0f, 1.0f	//3
+			   -50.0f, -50.0f, 0.0f, 0.0f,	//A0
+				50.0f, -50.0f, 1.0f, 0.0f,	//A1
+				50.0f,  50.0f, 1.0f, 1.0f,	//A2
+			   -50.0f,  50.0f, 0.0f, 1.0f,	//A3
+			   // Batch Rendering
+			    /*50.0f, -50.0f, 0.0f, 0.0f,	//A0
+			   150.0f, -50.0f, 1.0f, 0.0f,	//A1
+			   150.0f,  50.0f, 1.0f, 1.0f,	//A2
+			    50.0f,  50.0f, 0.0f, 1.0f,	//A3*/
 		};
 		unsigned int indices[] = {
-			0,1,2,
-			2,3,0
+			0,1,2, //A
+			2,3,0, //A
+			// Batch Rendering
+			/*4,5,6, //B
+			6,7,4, //B*/
 		};
 		GLCall(glEnable(GL_BLEND));
 		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 		m_VAO = std::make_unique<VertexArray>();
-		m_VertexBuffer = std::make_unique<VertexBuffer>(positions, 4 * 4 * sizeof(float));
+		m_VertexBuffer = std::make_unique<VertexBuffer>(positions, 4 /* Batch Rendering 8*/ * 4 * sizeof(float));
 		VertexBufferLayout layout;
 		layout.Push<float>(2);
 		layout.Push<float>(2);
 		m_VAO->AddBuffer(*m_VertexBuffer, layout);
-		m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 6);
+		m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 6/* Batch Rendering 12*/);
 		m_Shader = std::make_unique<Shader>("res/shaders/Basic.shader");
 		m_Shader->Bind();
 		m_Shader->SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
